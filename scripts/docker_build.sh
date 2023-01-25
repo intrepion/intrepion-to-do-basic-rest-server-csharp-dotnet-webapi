cc9d4483b6dc6e53ac97a8042c527feda932ff83
Oliver Forral <intrepion@gmail.com>
2023-01-25 00:32:01 -0700

    Added Digital Ocean files.

---
+++ scripts/docker_build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker build --tag intrepion-to-do-basic-rest-server-csharp-dotnet-webapi --file Dockerfile .
---
+++ scripts/docker_run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker run -p 80:80 intrepion-to-do-basic-rest-server-csharp-dotnet-webapi
